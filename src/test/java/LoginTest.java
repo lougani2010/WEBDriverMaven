1c494929bbf2e150d222a32a5fa13b7978ae498f
lougani <lougani2010@gmail.com>
2019-08-03 13:14:29 -0400

    Inilial commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,40 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+
+	@BeforeSuite
+	public void setUp() {
+		
+		WebDriverManager.chromedriver().setup();
+		driver= new ChromeDriver();
+
+	}
+	
+
+	@Test
+	public void doLogin() throws InterruptedException {
+		
+		driver.get("http://gmail.com");
+		driver.findElement(By.cssSelector("[type='email']")).sendKeys("lougani2010@gmail.com");
+		driver.findElement(By.cssSelector(".RveJvd.snByac")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//*[@name='password']")).sendKeys("bouchra83");
+		driver.findElement(By.cssSelector(".RveJvd.snByac")).click();
+
+	}
+	@AfterSuite
+	public void tearDown() {
+		
+		driver.quit();
+	}
+
+}
